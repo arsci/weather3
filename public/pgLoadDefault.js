0561e48dfe43ead54a7cd5d0dcdfdd8905c66a3b
arsci <russerya@gmail.com>
2016-08-24 09:34:40 -0700

    Add files via upload

--- public/pgLoadDefault.js
+++ public/pgLoadDefault.js
@@ -6,7 +6,7 @@ function pageLoadDefault(){
 	var da = currentDate.getDate();
 	
 	if(Number(mo) < 10) mo = "0" + mo;
-	if(Number(da) < 10) da = "0" = da;
+	if(Number(da) < 10) da = "0" + da;
 	
 	var urlAPI = "//api.wunderground.com/api/d95017df2847b211/conditions/forecast10day/" + yr + mo + da + "/q/94105.json";
 	
