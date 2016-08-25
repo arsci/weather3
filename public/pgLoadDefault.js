3ac286177775382bd8f84d524902891aa2919cb6
arsci <russerya@gmail.com>
2016-08-25 13:28:40 -0700

    Add files via upload

--- public/pgLoadDefault.js
+++ public/pgLoadDefault.js
@@ -19,7 +19,7 @@ function pageLoadDefault(){
 			$("#location").text(data.current_observation.observation_location.full);
 			$("#temp_f").text(data.current_observation.temperature_string);
 			$("#visibility_mi").text(data.current_observation.visibility_mi + 'mi');
-			$("#pressure_in").text(data.current_observation.pressure_in 'inHg');
+			$("#pressure_in").text(data.current_observation.pressure_in + 'inHg');
 			$("#observation_time").text(data.current_observation.observation_time);
 			$("#relative_humidity").text(data.current_observation.relative_humidity);
 			$("#wind_string").text(data.current_observation.wind_string);
