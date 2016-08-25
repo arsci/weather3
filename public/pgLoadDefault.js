2a3e7e3c87d58e57ccb3f7fa58280e1d18eaa1c9
arsci <russerya@gmail.com>
2016-08-25 11:01:39 -0700

    Add files via upload

--- public/pgLoadDefault.js
+++ public/pgLoadDefault.js
@@ -66,7 +66,7 @@ function pageLoadDefault(){
 			for(var l=0; l<12; l++) toHTML = toHTML + "<td>" + dataHistoricalVis[l] + "</td>";
 			toHTML = toHTML + "</tr></table>";
 			
-			toHTML = toHTML + "<table><tr><th>Time: </th>";
+			toHTML = toHTML + "<br><table><tr><th>Time: </th>";
 			for(var l=12; l<24; l++) toHTML = toHTML + "<th>" + labelsHistorical[l] + "</th>";
 			toHTML = toHTML + "</tr><tr><th>Sky:  </th>";
 			for(var l=12; l<24; l++) toHTML = toHTML + "<td>" + dataHistoricalSky[l] + "</td>";
