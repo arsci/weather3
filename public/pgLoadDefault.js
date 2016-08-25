76b92d0bd16775065d0dc4b5be2d45b12bbb09d0
arsci <russerya@gmail.com>
2016-08-25 15:35:06 -0700

    Add files via upload

--- public/pgLoadDefault.js
+++ public/pgLoadDefault.js
@@ -9,8 +9,7 @@ function pageLoadDefault(){
 	if(Number(da) < 10) da = "0" + da;
 	
 	var urlAPI = "//api.wunderground.com/api/d95017df2847b211/conditions/forecast10day/history_" 
-				 + yr + mo + da + 
-				 "/q/94105.json";
+				 + yr + mo + da + "/q/94105.json";
 
 	$.ajax({
 		type: 'GET',
