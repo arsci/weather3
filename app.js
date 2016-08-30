ccea325ca0c36826a51ea411fb85069fbaa5f7d8
arsci <russerya@gmail.com>
2016-08-30 15:08:33 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-var express = require('express');
-var request = require('request');
-
-var app = express();
-
-var weather_host = "http://api.wunderground.com/api/d95017df2847b211"
-
-function weatherAPI(path, qs, done) {
-    var url = weather_host + path;
+    var url = "http://api.wunderground.com/api/d95017df2847b211/conditions/forecast10day/history_20160829/q/94105.json";
     console.log(url, qs);
     request({
         url: url,
@@ -33,23 +25,5 @@ function weatherAPI(path, qs, done) {
             }
         }
     });
-}
 
-app.get('/api/weather', function(req, res) {
-    	weatherAPI("/conditions/forecast10day/history_20160829/q/94105.json", {
-        units: req.query.units || "e",
-        language: req.query.language || "en"
-    }, function(err, result) {
-        if (err) {
-        	console.log(err);
-            res.send(err).status(400);
-        } else {
-        	console.log("10 days Forecast");
-            res.json(result);
-        }
-    });
-});
 
-app.listen(appEnv.port, appEnv.bind, function() {
-  console.log("server starting on " + appEnv.url);
-});
