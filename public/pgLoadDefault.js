7b28f6b70e109c0c5cd25e4a2b4b576fa8bcf480
arsci <russerya@gmail.com>
2016-08-31 15:51:48 -0700

    Update pgLoadDefault.js

--- public/pgLoadDefault.js
+++ public/pgLoadDefault.js
@@ -11,7 +11,7 @@ function pageLoadDefault(){
 		for (var i=0; i<28; i++){
 		
 			a += "<tr><td>" + data.history.observations[i].date.hour + ":" + data.history.observations[i].date.min;
-			a += "</td><td>" + data.history.obeservations[i].tempi + "</td><td>" + data.history.obeservations[i].conds;
+			a += "</td><td>" + data.history.observations[i].tempi + "</td><td>" + data.history.observations[i].conds;
 			a += "</td></tr>";
 		
 		}
