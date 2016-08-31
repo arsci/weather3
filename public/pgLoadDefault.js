0b5cdc9b9e74b07248efa80a20a29ba6311c4812
arsci <russerya@gmail.com>
2016-08-31 15:54:37 -0700

    Update pgLoadDefault.js

--- public/pgLoadDefault.js
+++ public/pgLoadDefault.js
@@ -8,7 +8,7 @@ function pageLoadDefault(){
 	
 		var a = "<table>";
 		
-		for (var i=0; i<28; i++){
+		for (var i=0; i<30; i++){
 		
 			a += "<tr><td>" + data.history.observations[i].date.hour + ":" + data.history.observations[i].date.min;
 			a += "</td><td>" + data.history.observations[i].tempi + "</td><td>" + data.history.observations[i].conds;
