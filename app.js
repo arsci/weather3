f0040d8c174e51e9d21dfe31c4e451dc84e48039
arsci <russerya@gmail.com>
2016-08-31 20:37:32 -0700

    Update app.js

--- app.js
+++ app.js
@@ -37,11 +37,11 @@ function weatherAPI(path, done) {
 }
 
 app.get('/api/weather', function(req, res) {
-	var day = req.param('date');
-  	var zipcode = req.param('zip');	
-  	res.redirect("/");
+	//var day = req.param('date');
+  	//var zipcode = req.param('zip');	
+  	//res.redirect("/");
   	
-    	weatherAPI("/history_" + "20160835" + "/q/" + "94523" + ".json", function(err, result) {
+    	weatherAPI("/history_" + "20160825" + "/q/" + "94523" + ".json", function(err, result) {
         if (err) {
         	console.log(err);
             res.send(err).status(400);
