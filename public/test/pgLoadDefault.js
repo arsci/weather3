484a221d6517c718ec3cf3f6ee49c4b13b7cc93a
arsci <russerya@gmail.com>
2016-08-31 20:19:28 -0700

    Create pgLoadDefault.js

---
+++ public/test/pgLoadDefault.js
@@ -0,0 +1,25 @@
+function pageLoadDefault(){			
+	
+	var urlAPI = "/api/weather";
+		$.ajax({
+		type: 'GET',
+		url: urlAPI,
+		success: function(data) {	
+	
+		var a = "<table>";
+		
+		for (var i=0; i<24; i++){
+		
+			a += "<tr><td>" + data.history.observations[i].date.hour + ":" + data.history.observations[i].date.min;
+			a += "</td><td>" + data.history.observations[i].tempi + "</td><td>" + data.history.observations[i].conds;
+			a += "</td></tr>";
+		
+		}
+		
+		a += "</table>";
+		
+		document.getElementById("outputdata").innerHTML = a;
+		
+		}
+	})
+}		
